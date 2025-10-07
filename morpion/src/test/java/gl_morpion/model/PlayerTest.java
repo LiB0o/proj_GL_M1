91ce2ddff9ca9690ac9c1872b728d8bc3eb90bbb
lison <lisonboo9@gmail.com>
2025-10-07 16:56:26 +0200

    PlayerTest.java getName and setName test successfully implemented

--- morpion/src/test/java/gl_morpion/model/PlayerTest.java
+++ morpion/src/test/java/gl_morpion/model/PlayerTest.java
@@ -1,5 +1,7 @@
 package gl_morpion.model;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +10,20 @@ class PlayerTest {
 
     @Test
     void getName() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",croix);
+
+        assertEquals("AAA",test.getName());
     }
 
     @Test
     void setName() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",croix);
+
+        assertEquals("AAA",test.getName());
+        test.setName("BBB");
+        assertEquals("BBB",test.getName());
     }
 
     @Test
