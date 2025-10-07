115af38bb9ef2d03b773f556b47da00b4b39c178
lison <lisonboo9@gmail.com>
2025-10-07 17:03:38 +0200

    PlayerTest.java isTurn, waitTurn, playTurn, addPoint, setPoints, getPoints tests successfully implemented

--- morpion/src/test/java/gl_morpion/model/PlayerTest.java
+++ morpion/src/test/java/gl_morpion/model/PlayerTest.java
@@ -28,25 +28,61 @@ class PlayerTest {
 
     @Test
     void getPoints() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",0,croix);
+
+        assertEquals(0,test.getPoints());
+
     }
 
     @Test
     void setPoints() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",0,croix);
+
+        assertEquals(0,test.getPoints());
+        test.setPoints(10);
+        assertEquals(10,test.getPoints());
     }
 
     @Test
     void addPoint() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",0,croix);
+
+        assertEquals(0,test.getPoints());
+        test.addPoint();
+        assertNotEquals(10,test.getPoints());
+        assertEquals(1,test.getPoints());
     }
 
     @Test
     void playTurn() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",croix);
+
+        assertFalse(test.isTurn());
+        test.playTurn();
+        assertTrue(test.isTurn());
     }
 
     @Test
     void waitTurn() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",croix);
+
+        assertFalse(test.isTurn());
+        test.playTurn();
+        assertTrue(test.isTurn());
+        test.waitTurn();
+        assertFalse(test.isTurn());
     }
 
     @Test
     void isTurn() {
+        Symbol croix = new Symbol("croix.pnj",TypeOfSymbol.CROSS);
+        Player test = new Player("AAA",croix);
+
+        assertFalse(test.isTurn());
     }
 }
