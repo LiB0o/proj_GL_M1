e8e4cdfa52d47724ed7cfa7f4ae53f07cad58e76
lison <lisonboo9@gmail.com>
2025-10-07 16:38:27 +0200

    add PlayerTest.java

---
+++ morpion/src/test/java/gl_morpion/model/PlayerTest.java
@@ -0,0 +1,40 @@
+package gl_morpion.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getPoints() {
+    }
+
+    @Test
+    void setPoints() {
+    }
+
+    @Test
+    void addPoint() {
+    }
+
+    @Test
+    void playTurn() {
+    }
+
+    @Test
+    void waitTurn() {
+    }
+
+    @Test
+    void isTurn() {
+    }
+}
