e510c69ee0f6697fe69eccacfbcf2691898094c6
lison <lisonboo9@gmail.com>
2025-10-07 14:10:02 +0200

    Add GameBoard and RectangleBoard with their JavaDoc

--- morpion/src/main/java/gl/morpion/model/RectangleBoard.java
+++ morpion/src/main/java/gl/morpion/model/RectangleBoard.java
@@ -1,9 +1,14 @@
 package gl.morpion.model;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RectangleBoard extends GameBoard {
 
-	private int DEFAULT_COLUMN;
-	private int DEFAULT_ROW;
+	private int DEFAULT_COLUMN = 30;
+	private int DEFAULT_ROW = 30;
 
 	public int getDEFAULT_COLUMN() {
 		return this.DEFAULT_COLUMN;
@@ -14,14 +19,33 @@ public class RectangleBoard extends GameBoard {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param x
 	 * @param y
 	 */
 	public RectangleBoard(int x, int y) {
         super(x, y);
-        // TODO - implement RectangleBoard.RectangleBoard
-		throw new UnsupportedOperationException();
+
+		super.setUseCase(setListRectangle(x,y));
 	}
+
+
+	/**
+	 * Generate the list of cases the user can play on
+	 * @param row
+	 * @param column
+	 * @return
+	 */
+	public List<Pair<Integer, Integer>> setListRectangle(int row, int column){
+		List<Pair<Integer, Integer>> list = new ArrayList<Pair<Integer, Integer>>();
+
+		for(int i =0;i<row;i++){
+			for(int j=0;j<column;j++){
+				list.add(new Pair<>(i,j));
+			}
+		}
+		return list;
+	}
+
 
 }
