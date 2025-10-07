8fd909467cfb08f0f59e076eca18b652718ee23e
lison <lisonboo9@gmail.com>
2025-10-07 14:10:02 +0200

    Add GameBoard and RectangleBoard with their JavaDoc

--- morpion/src/main/java/gl/morpion/model/GameBoard.java
+++ morpion/src/main/java/gl/morpion/model/GameBoard.java
@@ -9,72 +9,119 @@ import java.util.*;
  */
 public abstract class GameBoard {
 
-	List<List<Symbol>> symbols;
+	public List<Symbol[]> symbols;
 	private int column;
 	private int row;
-	private List<Pair<Integer, Integer>> useCase;
+	public List<Pair<Integer, Integer>> useCase;
 
+	/**
+	 * Get the number of column of the Board
+	 * @return int
+	 */
 	public int getColumn() {
 		return this.column;
 	}
 
+	/**
+	 *
+	 * @param column
+	 */
 	public void setColumn(int column) {
-		this.column = column;
+		this.column = column; //Maybe useless
 	}
 
+	/**
+	 * Get the number of row of the Board
+	 * @return int
+	 */
 	public int getRow() {
 		return this.row;
 	}
 
+	/**
+	 *
+	 * @param row
+	 */
 	public void setRow(int row) {
-		this.row = row;
+		this.row = row; //Maybe useless
 	}
 
 	/**
-	 *  
-	 *  
+	 *  Place a symbol inside the List<Symbol[]> symbols
+	 *  Return a true if the symbol was inserted
+	 *
 	 * @param symbol
-	 * @param x
-	 * @param y
+	 * @param x : line of the board
+	 * @param y : column of the board
 	 */
 	protected boolean placeSymbol(Symbol symbol, int x, int y) {
-		return true;
+
+		boolean result = false;
+
+		if(this.isValidCase(x,y) && this.isEmptyCase(x,y)){
+			this.symbols.get(x)[y] = symbol;
+			result = true;
+		}
+
+		return result;
 	}
 
 	/**
-	 * determine whether a box is valid or not
+	 * Determine whether a box is valid or not
 	 * @param x
 	 * @param y
 	 */
 	protected boolean isValidCase(int x, int y) {
-		return true;
+		Pair<Integer, Integer> test = new Pair<>(x,y);
+		return this.useCase.contains(test);
 	}
 
 	/**
-	 * allows you to get a symbol in a given box.
+	 * Allows you to get a symbol in a given box.
 	 * @param row
 	 * @param col
 	 */
 	protected Symbol getSymbolInCase(int row, int col) {
-		return null;
+		return this.symbols.get(row)[col];
 	}
 
 	/**
-	 * 
-	 * @param x
-	 * @param y
+	 * Constructor of GameBoard, cannot be used without children
+	 * @param column
+	 * @param row
 	 */
-	public GameBoard(int x, int y) {
+	public GameBoard(int row,int column) {
 
+		this.row = row;
+		this.column = column;
+
+		List<Symbol[]> tab = new ArrayList<Symbol[]>();
+		for(int i=0;i<row;i++){
+			tab.add(new Symbol[column]);
+		}
+		this.symbols = tab;
 	}
 
 	/**
-	 * 
-	 * @param x
-	 * @param y
+	 * Set useCase (will be call inside the children constructor)
+	 * @param listUse : list of case where the player can play (given by the children)
+	 */
+	protected void setUseCase(List<Pair<Integer, Integer>> listUse){
+		this.useCase = listUse;
+	}
+
+	/**
+	 * Return if the place selected is empty
+	 * For test purpose, will print the state of the case
+	 * @param x : row
+	 * @param y : column
 	 */
 	protected boolean isEmptyCase(int x, int y) {
-		return true; // a supprimer
+
+		Symbol[] line = this.symbols.get(x);
+		System.out.println("GameBoard isEmptyCase :"+line[y]); //test
+
+        return (line[y] == null);
 	}
 
 }
