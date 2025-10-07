3d88f7234765cd9b9efa11ddd257e2b8e4a5ab3e
lison <lisonboo9@gmail.com>
2025-10-07 14:32:32 +0200

    RectangleBoard.java add default RectangleBoard()

--- morpion/src/main/java/gl/morpion/model/RectangleBoard.java
+++ morpion/src/main/java/gl/morpion/model/RectangleBoard.java
@@ -5,18 +5,12 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class RectangleBoard extends GameBoard {
 
-	private int DEFAULT_COLUMN = 30;
-	private int DEFAULT_ROW = 30;
-
-	public int getDEFAULT_COLUMN() {
-		return this.DEFAULT_COLUMN;
-	}
-
-	public int getDEFAULT_ROW() {
-		return this.DEFAULT_ROW;
-	}
+	final private int DEFAULT_COLUMN = 30;
+	final private int DEFAULT_ROW = 30;
 
 	/**
 	 *
@@ -27,6 +21,15 @@ public class RectangleBoard extends GameBoard {
         super(x, y);
 
 		super.setUseCase(setListRectangle(x,y));
+	}
+
+	/**
+	 * Default constructor
+	 */
+	public RectangleBoard(){
+		super(30,30);
+
+		super.setUseCase(setListRectangle(30,30));
 	}
 
 
