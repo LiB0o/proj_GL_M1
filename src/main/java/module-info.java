f32f4bc8ad5fc90f776b6036260f3805b17a4fd9
lison <lisonboo9@gmail.com>
2025-10-14 18:13:12 +0200

    refonte de main branch

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module gl.morpion {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires javafx.base;
+
+    opens gl.morpion to javafx.fxml;
+    exports gl.morpion;
+    exports gl.morpion.controllers;
+    opens gl.morpion.controllers to javafx.fxml;
+}
