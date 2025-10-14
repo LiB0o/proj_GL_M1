ff3e13f22d8a82a832a2b6ff738193eaaaf19476
lison <lisonboo9@gmail.com>
2025-10-14 14:43:43 +0200

    creation of every fonction + documentation: Game.Game Game.checkVictory Game.checkColumn Game.checkLine Game.checkDiagonalUpLeft_DownRight Game.checkDiagonalUpRight_DownLeft
    In checkVictory need to call GameBoard to set usedCase

--- morpion/src/main/java/gl/morpion/model/Game.java
+++ morpion/src/main/java/gl/morpion/model/Game.java
@@ -1,26 +1,300 @@
 package gl.morpion.model;
 
+import javafx.util.Pair;
+
 import java.util.*;
 
 public class Game {
 
+	private HashMap<Pair<Integer,Integer>,Symbol> usedCase;
 	private GameBoard gameBoard;
 	List<Player> players;
 
+	/**
+	 * Create the instance of Game
+	 * @param board of type GameBoard
+	 * @param players as a List<Player>
+	 */
 	public Game(GameBoard board,List<Player> players){
 		this.gameBoard = board;
 		this.players = new ArrayList<>();
 
 		this.players.addAll(players);
+		this.usedCase = new HashMap<>();
 	}
 
-	public Game(){
+	/**
+	 * test every possibility for a case to check if there is a win
+	 * @param limit how many symbols are needed next to each other to win
+	 * @return boolean
+	 */
+	public boolean checkVictory(int limit) {
+		boolean victory = false;
 
+		//TODO: get the HashMap<> from the GameBoard fonction
+
+		for(Pair<Integer,Integer>key : this.usedCase.keySet()){
+			victory = victory || (checkColumn(key,limit)||
+					checkDiagonalUpLeft_DownRight(key,limit)||
+					checkDiagonalUpRight_DownLeft(key,limit)||
+					checkLine(key,limit));
+		}
+
+		return victory;
 	}
 
-	public boolean checkVictory() {
-		// TODO - implement Game.checkVictory
-		throw new UnsupportedOperationException();
+	/**
+	 * check if you win with on a diagonal from up left to down right
+	 * @param key position of case, first Integer is x/line and the second Integer is y/column
+	 * @param limit how many symbols are needed next to each other to win
+	 * @return boolean
+	 */
+	private boolean checkDiagonalUpLeft_DownRight(Pair<Integer,Integer>key,int limit){
+		boolean result = false;
+		boolean folowed = true;
+		int verif = 0; // permit to check if the column with
+		//Pair<Integer,Integer> x = line, y = column
+		Symbol testedSymbol = usedCase.get(key); //Symbol of the case you test victory
+
+		if(testedSymbol != null){
+			for(int i=1;i<limit;i++){ //behind (left)
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey()-i, key.getValue()-i))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey()-i, key.getValue()-i))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+
+			folowed = true;
+			for(int i=1;i<limit;i++){ //forward
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey()+i, key.getValue()+i))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey()+i, key.getValue()+i))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+		}
+
+		if(verif >= limit){ //if we win
+			result = true;
+		}
+
+		return result;
+	}
+
+	/**
+	 * check if you win with on a diagonal from down left to up right
+	 * @param key position of case, first Integer is x/line and the second Integer is y/column
+	 * @param limit how many symbols are needed next to each other to win
+	 * @return boolean
+	 */
+
+	private boolean checkDiagonalUpRight_DownLeft(Pair<Integer,Integer>key,int limit){
+		boolean result = false;
+		boolean folowed = true;
+		int verif = 0; // permit to check if the column with
+		//Pair<Integer,Integer> x = line, y = column
+		Symbol testedSymbol = usedCase.get(key); //Symbol of the case you test victory
+
+		if(testedSymbol != null){
+			for(int i=1;i<limit;i++){ //behind (left)
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey()+i, key.getValue()-i))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey()+i, key.getValue()-i))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+
+			folowed = true;
+			for(int i=1;i<limit;i++){ //forward (right)
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey()-i, key.getValue()+i))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey()-i, key.getValue()+i))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+		}
+
+		if(verif >= limit){ //if we win
+			result = true;
+		}
+
+		return result;
+	}
+
+	/**
+	 * check if you win with on a column
+	 *
+	 * @param key position of case, first Integer is x/line and the second Integer is y/column
+	 * @param limit how many symbols are needed next to each other to win
+	 * @return boolean
+	 */
+	private boolean checkColumn(Pair<Integer,Integer> key ,int limit){
+		boolean result = false;
+		boolean folowed = true;
+		int verif = 0; // permit to check if the column with
+		//Pair<Integer,Integer> x = line, y = column
+		Symbol testedSymbol = usedCase.get(key); //Symbol of the case you test victory
+
+		if(testedSymbol != null){
+			for(int i=1;i<limit;i++){ //behind
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey(), key.getValue()-i))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey(), key.getValue()-i))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+
+			folowed = true;
+			for(int i=1;i<limit;i++){ //forward
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey(), key.getValue()+i))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey(), key.getValue()+i))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+		}
+
+		if(verif >= limit){ //if we win
+			result = true;
+		}
+
+		return result;
+	}
+
+	/**
+	 * check if you win with on a line
+	 * @param key position of case, first Integer is x/line and the second Integer is y/column
+	 * @param limit how many symbols are needed next to each other to win
+	 * @return boolean
+	 */
+	private boolean checkLine(Pair<Integer,Integer>key,int limit){
+		boolean result = false;
+		boolean folowed = true;
+		int verif = 0; // permit to check if the column with
+		//Pair<Integer,Integer> x = line, y = column
+		Symbol testedSymbol = usedCase.get(key); //Symbol of the case you test victory
+
+		if(testedSymbol != null){
+			for(int i=1;i<limit;i++){ //behind
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey()-i, key.getValue()))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey()-i, key.getValue()))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+
+			folowed = true;
+			for(int i=1;i<limit;i++){ //forward
+				if(usedCase.containsKey(
+						new Pair<Integer,Integer>(key.getKey()+i, key.getValue()))){ //if the case exist
+					if(testedSymbol == usedCase.get(
+							new Pair<Integer,Integer>(key.getKey()+i, key.getValue()))){ //if it have the same symbol
+						verif += 1;
+					}
+					else{
+						folowed = false;
+					}
+				}
+				else{
+					folowed = false;
+				}
+
+				if(folowed == false){
+					i = limit;
+				}
+			}
+		}
+
+		if(verif >= limit){ //if we win
+			result = true;
+		}
+
+		return result;
 	}
 
 	/**
