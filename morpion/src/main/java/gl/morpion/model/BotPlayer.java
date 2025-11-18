8df6adfca269b6315234b7558d22703829a6b2ec
lison <lisonboo9@gmail.com>
2025-11-18 08:57:32 +0100

    BotPlayer ajout d'un BotPlayer avec un seul coeff plus ses fonctions getLevel et setLevel Fin de la v1 de verticalValueOfCase, pour test essayer la case (0,0) avec un coef de 3.5 le résultat devrait être de 1

--- morpion/src/main/java/gl/morpion/model/BotPlayer.java
+++ morpion/src/main/java/gl/morpion/model/BotPlayer.java
@@ -11,6 +11,9 @@ public class BotPlayer extends Player {
 	private float coef_attack; // Used to calculate the best move offensively
 	private float coef_defence; //Used to calculate the best move defensively
 
+
+	private float full_coef;
+
 	public int win_condition;
 
 	/*
@@ -26,6 +29,17 @@ public class BotPlayer extends Player {
 
 	////////////////////////////////////////////////////////////////////////////////////////////
 
+	/**
+	 * Create the Bot with 2 different coeff
+	 * Please DO NOT use this version for the game
+	 *
+	 * @param name
+	 * @param point
+	 * @param attack
+	 * @param defence
+	 * @param symbol
+	 * @param limit
+	 */
 	public BotPlayer(String name,
 					 int point,
 					 float attack,
@@ -41,6 +55,19 @@ public class BotPlayer extends Player {
 		this.boardView = new HashMap<Pair<Integer,Integer>,Integer>(); //Init with function setBotBoard
 	}
 
+	public BotPlayer(String name,
+					 int point,
+					 float coef,
+					 Symbol symbol,
+					 int limit) {
+		super(name, point,symbol);
+
+		this.full_coef = coef;
+
+		this.win_condition = limit;
+		this.boardView = new HashMap<Pair<Integer,Integer>,Integer>(); //Init with function setBotBoard
+	}
+
 
 	public void setBotBoard(List<Pair<Integer, Integer>> usableCases){
 		for(Pair<Integer,Integer> p : usableCases){
@@ -48,17 +75,78 @@ public class BotPlayer extends Player {
 		}
 	}
 
-	public Pair<Float,Float> getLevel() {
+	public Pair<Float,Float> getCoefs() {
 		return new Pair<>(coef_attack, coef_defence);
 	}
 
-	public void setLevel(float attack, float defence) {
+	public float getLevel(){
+		return this.full_coef;
+	}
+
+	public void setLevels(float attack, float defence) {
 		this.coef_attack = attack;
 		this.coef_defence = defence;
 	}
 
+	public void setLevel(float coef){
+		this.full_coef = coef;
+	}
+
 	//Calcul vertical d'une case
 
+	private float verticalValueOfCase(Pair<Integer, Integer> position){
+
+		float value = 1.0f;
+
+		//Check the possibilities before and after the chosen point
+		for(int i = (position.getValue()-(this.win_condition+1)); i < position.getValue()+1; i++){
+			int nb_bot_symbol = 0; //number of time where the symbol of the bot is present
+			int nb_adverse_symbol = 0; //number of time where the symbol of the enemy is present
+			if(this.boardView.containsKey(position)){ //If the case is present
+
+				for(int j = 0; j <= this.win_condition; j++){
+					Pair<Integer, Integer> testCase = new Pair<>(position.getKey(),i+j);
+					if(this.boardView.get(testCase) == -1){ //If the case has a symbol from the adverser
+						nb_adverse_symbol += 1;
+					}
+					if(this.boardView.get(testCase) == 0){ //If the case has a symbol from the adverser
+						nb_bot_symbol += 1;
+					}
+
+					/*if(nb_bot_symbol != 0 && nb_adverse_symbol !=0){ //if closed add nothing to value and check the next posibility
+						j = this.win_condition*2; //to stop the for
+					}*/
+				}
+				if(nb_bot_symbol != 0 && nb_adverse_symbol !=0){ //if closed add nothing to value and check the next posibility
+					value = value;
+				}
+				else{
+					if(nb_bot_symbol !=0){
+
+						float add = 3.0f;
+
+						for(int compt = 0; compt<nb_bot_symbol; compt++){
+							add = add * this.full_coef;
+						}
+
+						value = value + add;
+					}
+					else{
+						float add = 2.0f;
+
+						for(int compt = 0; compt<nb_adverse_symbol; compt++){
+							add = add * this.full_coef;
+						}
+
+						value = value + add;
+					}
+				}
+
+				
+			}
+		}
+		return value;
+	}
 
 	//Calcul horizontal d'une case
 
