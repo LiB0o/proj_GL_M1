a5d1ce5f1b91165174c2df2e51bebbfb3569c007
lison <lisonboo9@gmail.com>
2025-11-24 11:28:52 +0100

    Complete Javadoc of RectangleBoard

--- morpion/src/main/java/gl/morpion/model/RectangleBoard.java
+++ morpion/src/main/java/gl/morpion/model/RectangleBoard.java
@@ -6,12 +6,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * <h1>class RectangleBoard</h1>
+ * <h2>Elements of RectangleBoard</h2>
+ */
+
 public class RectangleBoard extends GameBoard {
 
 	//private int DEFAULT_COLUMN = 30;
 	//private int DEFAULT_ROW = 30;
 
-	public static final int DEFAULT_ROW = 10, DEFAULT_COLUMN = 10; //TODO:  probleme si ROW defferent COL
+	public static final int DEFAULT_ROW = 10, DEFAULT_COLUMN = 10;
+
+	/**
+	 * <h2>Functions of RectangleBoard</h2>
+	 */
+
 
 	public int getDEFAULT_COLUMN() {
 		return this.DEFAULT_COLUMN;
@@ -22,9 +32,10 @@ public class RectangleBoard extends GameBoard {
 	}
 
 	/**
-	 *
-	 * @param x
-	 * @param y
+	 * <h3>RectangleBoard</h3>
+	 * Constructor of RectangleBoard, call its parent "GameBoard"
+	 * @param x rows
+	 * @param y columns
 	 */
 	public RectangleBoard(int x, int y) {
         super(x, y);
@@ -34,10 +45,11 @@ public class RectangleBoard extends GameBoard {
 
 
 	/**
+	 * <h3>setListRectangle</h3>
 	 * Generate the list of cases the user can play on
 	 * @param row
 	 * @param column
-	 * @return
+	 * @return the list of cases the user can play on
 	 */
 	public List<Pair<Integer, Integer>> setListRectangle(int row, int column){
 		List<Pair<Integer, Integer>> list = new ArrayList<Pair<Integer, Integer>>();
